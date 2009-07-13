9f855e74ddd9eabb719d54c15e69d2b924602955
Neelakanth Nadgir <realneel@gmail.com>
2009-07-13 17:43:22 +0000

    ability to override uperf binary on command line

--- tests/test.sh
+++ tests/test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export host=${h:-localhost}
-echo h=$host duration=10s $1 -x -m $2 >>log
-h=$host duration=10s $1 -x -m $2 >> log 2>&1
+export uperf=${UPERF:-$1}
+echo h=$host duration=10s $uperf -x -m $2 >>log
+h=$host duration=10s $uperf -x -m $2 >> log 2>&1
 
 
